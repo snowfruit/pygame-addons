ee192cc5954b2a79c8fab27ed350ee99b7e1c8be
snowfruit <144597172+snowfruit@users.noreply.github.com>
2024-04-03 13:44:25 +0200

    Add files via upload
    First draft.

--- pygame_customcanvas/example_1.py
+++ pygame_customcanvas/example_1.py
@@ -1,1 +1,46 @@
-# Hello, World!
+import math
+import pygame
+
+from pygame_customcanvas import *
+
+
+def main():
+    pygame.init()
+    clock = pygame.Clock()
+
+    # Window, screen and canvas use the same size.
+    window_size = (320, 240)
+
+    # Screen is a resizable window.
+    screen = pygame.display.set_mode(window_size, pygame.RESIZABLE)
+
+    # Set canvas-size to same as starting screen-size.
+    cc = CustomCanvas(window_size)
+
+    pygame.display.set_caption("Pygame - Retro scaling")
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        cc.update()
+
+        # Render a circle to demonstrate the scaling.
+        pygame.draw.circle(cc.canvas, (255, 255, 255), (160, 120), 32)
+
+        # Render the canvas-surface in the center of screen-surface.
+        cc.blit_canvas_to_screen()
+
+        pygame.display.flip()
+
+        clock.tick(30)
+        # print(rc.get_mouse_position())
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
